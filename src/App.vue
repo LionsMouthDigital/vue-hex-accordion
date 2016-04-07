2892312fcf8178d19f3291da2ed7f8348d7971c3
Curtis Blackwell <blackwell.curtis@gmail.com>
2016-04-07 16:08:11 +0900

    :sparkles: use webpack

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,44 @@
 <template>
   <div id="app">
+    <h1>Hex Accordion</h1>
+    <hex-accordions>
+      <hex-accordion>
+        <hex-accordion-tab><h3>Bali</h3></hex-accordion-tab>
+        <hex-accordion-panel>
+          <p>Land o' da hexes.</p>
+        </hex-accordion-panel>
+      </hex-accordion>
+
+      <hex-accordion>
+        <hex-accordion-tab><h3>Dallas</h3></hex-accordion-tab>
+        <hex-accordion-panel>
+          <p>Land o' da Texas.</p>
+        </hex-accordion-panel>
+      </hex-accordion>
+    </hex-accordions>
+
+
+    <h2>Open on load</h2>
     <hex-accordions>
       <hex-accordion active>
+        <hex-accordion-tab><h3>Bali</h3></hex-accordion-tab>
+        <hex-accordion-panel>
+          <p>Land o' da hexes.</p>
+        </hex-accordion-panel>
+      </hex-accordion>
+
+      <hex-accordion>
+        <hex-accordion-tab><h3>Dallas</h3></hex-accordion-tab>
+        <hex-accordion-panel>
+          <p>Land o' da Texas.</p>
+        </hex-accordion-panel>
+      </hex-accordion>
+    </hex-accordions>
+
+
+    <h2>All the single ladies</h2>
+    <hex-accordions single>
+      <hex-accordion>
         <hex-accordion-tab><h3>Bali</h3></hex-accordion-tab>
         <hex-accordion-panel>
           <p>Land o' da hexes.</p>
--- src/index.js
+++
@@ -1,4 +0,0 @@
-exports.HexAccordions     = require('./components/HexAccordions.vue');
-exports.HexAccordion      = require('./components/HexAccordion.vue');
-exports.HexAccordionTab   = require('./components/HexAccordionTab.vue');
-exports.HexAccordionPanel = require('./components/HexAccordionPanel.vue');
